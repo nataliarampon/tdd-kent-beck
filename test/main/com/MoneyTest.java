1200f295aff77bf84412930f5f26aee47270e039
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-04-14 22:35:17 -0300

    add failing test for serial multiplication

--- test/main/com/MoneyTest.java
+++ test/main/com/MoneyTest.java
@@ -7,7 +7,10 @@ class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
         five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
