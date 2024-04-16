4854442104fdfa36b3da1965fb85e8ed106a84d3
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-04-15 22:12:26 -0300

    create Money superclass tests

---
+++ test/main/com/DollarTest.java
@@ -0,0 +1,13 @@
+package main.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DollarTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+}
--- test/main/com/MoneyTest.java
+++ test/main/com/MoneyTest.java
@@ -1,19 +1,15 @@
 package main.com;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class MoneyTest {
-    @Test
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
-    }
+import static org.junit.jupiter.api.Assertions.*;
 
+public class MoneyTest {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
