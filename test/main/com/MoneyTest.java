d1eeba4a36e647ae5a41bff5aa7caf4c33e56527
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-04-19 09:51:48 -0300

    create test for same-currency addition

--- test/main/com/MoneyTest.java
+++ test/main/com/MoneyTest.java
@@ -24,4 +24,10 @@ public class MoneyTest {
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
+
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
