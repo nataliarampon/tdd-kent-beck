84fed5eb9bb7598df62af52381f165b504a319d5
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-04-20 10:23:56 -0300

    create test for reducing with exchange rates

--- test/main/com/BankTest.java
+++ test/main/com/BankTest.java
@@ -21,4 +21,16 @@ class BankTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
