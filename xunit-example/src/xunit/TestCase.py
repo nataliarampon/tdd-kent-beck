1e50debef69a3e2b94e310a39e11821009624d6f
nataliarampon <nataliarampon@gmail.com>
2024-06-19 19:29:14 -0300

    create set up method in test case

--- xunit-example/src/xunit/TestCase.py
+++ xunit-example/src/xunit/TestCase.py
@@ -1,7 +1,11 @@
 class TestCase(object):
   def __init__(self, name):
     self.name = name
+  
+  def setUp(self):
+    pass
 
   def run(self):
+    self.setUp()
     method = getattr(self, self.name)
     return method()
--- xunit-example/src/xunit/WasRun.py
+++ xunit-example/src/xunit/WasRun.py
@@ -3,7 +3,11 @@ from .TestCase import TestCase
 class WasRun(TestCase):
   def __init__(self, name):
     self.wasRun = False
+    self.wasSetUp = False
     TestCase.__init__(self, name)
+  
+  def setUp(self):
+    self.wasSetUp = True
 
   def testMethod(self):
     self.wasRun = True
