3c3aee40caaf3d0823dd19dbae4db0efe1c4ff38
nataliarampon <nataliarampon@gmail.com>
2024-06-19 19:26:15 -0300

    test for test set up

--- xunit-example/src/TestCaseTest.py
+++ xunit-example/src/TestCaseTest.py
@@ -2,6 +2,11 @@ from xunit.TestCase import TestCase
 from xunit.WasRun import WasRun
 
 class TestCaseTest(TestCase):
+  def testSetUp(self):
+    test = WasRun('testMethod')
+    test.run()
+    assert(test.wasSetUp)
+
   def testRunning(self):
     test = WasRun('testMethod')
     assert(not test.wasRun)
@@ -9,3 +14,4 @@ class TestCaseTest(TestCase):
     assert(test.wasRun)
   
 TestCaseTest('testRunning').run()
+TestCaseTest('testSetUp').run()
