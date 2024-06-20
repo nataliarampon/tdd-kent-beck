6b10b4e7397117cc248a252be92e8fa79ed542b6
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-06-20 16:48:32 -0300

    create fake implementation for failure report

--- xunit-example/src/xunit/TestResult.py
+++ xunit-example/src/xunit/TestResult.py
@@ -1,9 +1,13 @@
 class TestResult(object):
   def __init__(self):
     self.runCount = 0
+    self.errorCount = 0
 
   def testStarted(self):
     self.runCount += 1
+  
+  def testFailed(self):
+    self.errorCount += 1
 
   def summary(self):
-    return f'{self.runCount} run, 0 failed'
+    return f'{self.runCount} run, {self.errorCount} failed'
