4abcf195794f91ab1f9d7b4c0f64aec5b305bcd2
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-06-22 15:31:08 -0300

    create test for test suite

--- xunit-example/src/TestCaseTest.py
+++ xunit-example/src/TestCaseTest.py
@@ -1,31 +1,46 @@
 from xunit.TestCase import TestCase
 from xunit.WasRun import WasRun
 from xunit.TestResult import TestResult
+from xunit.TestSuite import TestSuite
 
 class TestCaseTest(TestCase):
   def setUp(self):
-    self.test = WasRun('testMethod')
+    self.result = TestResult()
 
-  def testSetUp(self):
-    self.test.run()
-    assert(self.test.log == 'setUp testMethod tearDown ')
+  def testOrder(self):
+    test = WasRun('testMethod')
+    test.run(self.result)
+    assert(test.log == 'setUp testMethod tearDown ')
   
   def testResult(self):
-    result = self.test.run()
-    assert('1 run, 0 failed' == result.summary())
+    test = WasRun('testMethod')
+    test.run(self.result)
+    assert('1 run, 0 failed' == self.result.summary())
 
   def testFailedResult(self):
     test = WasRun('testBrokenMethod')
-    result = test.run()
-    assert('1 run, 1 failed' == result.summary())
+    test.run(self.result)
+    assert('1 run, 1 failed' == self.result.summary())
   
   def testFailedResultFormatting(self):
     result = TestResult()
     result.testStarted()
     result.testFailed()
     assert('1 run, 1 failed' == result.summary())
+  
+  def testSuite(self):
+    suite = TestSuite()
+    suite.add(WasRun('testMethod'))
+    suite.add(WasRun('testBrokenMethod'))
+    suite.run(self.result)
+    assert('2 run, 1 failed' == self.result.summary())
 
-print(TestCaseTest('testSetUp').run().summary())
-print(TestCaseTest('testResult').run().summary())
-print(TestCaseTest('testFailedResult').run().summary())
-print(TestCaseTest('testFailedResultFormatting').run().summary())
+suite = TestSuite()
+suite.add(TestCaseTest('testOrder'))
+suite.add(TestCaseTest('testResult'))
+suite.add(TestCaseTest('testFailedResult'))
+suite.add(TestCaseTest('testFailedResultFormatting'))
+suite.add(TestCaseTest('testSuite'))
+result = TestResult()
+suite.run(result)
+print(result.summary())
